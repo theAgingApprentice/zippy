ed4dfd7961883f11518b899073bb8725521ef645
theAgingApprentice <theAgingApprentice@users.noreply.github.com>
2021-11-25 00:29:05 +0000

    deploy: c30cdab7b157ec7cfab8f5854605cc2dac6ae188

--- html/search/pages_0.js
+++ html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['_3ca_20href_3d_22https_3a_2f_2fgithub_2ecom_2ftheagingapprentice_2ficunderware_22_3eicunderwear_3c_2fa_3e_20_3c_26ndash_3b_20your_20next_20robot_20project_20start_20here_21_0',['&lt;a href=&quot;https://github.com/theAgingApprentice/icUnderware&quot;&gt;icUnderwear&lt;/a&gt; &lt;&amp;ndash; Your next robot project start here!',['../index.html',1,'']]]
+  ['_3ca_20href_3d_22https_3a_2f_2fgithub_2ecom_2ftheagingapprentice_2fzippy_22_3ezippy_20repository_3c_2fa_3e_20_3c_26ndash_3b_20experimental_20inverted_20pendulum_20robot_2e_0',['&lt;a href=&quot;https://github.com/theAgingApprentice/zippy&quot;&gt;Zippy repository&lt;/a&gt; &lt;&amp;ndash; Experimental inverted pendulum robot.',['../index.html',1,'']]]
 ];

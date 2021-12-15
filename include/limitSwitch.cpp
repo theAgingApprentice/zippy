1b8697a48943e7f200162a29478bdbc430f969e0
theAgingApprentice <TheAgingApprentice@protonmail.com>
2021-12-14 19:55:40 -0500

    Add diagrams and parts list to repository Fixes #24

--- include/limitSwitch.cpp
+++ include/limitSwitch.cpp
@@ -4,67 +4,101 @@
 
 #include <main.h> // Header file for all libraries needed by this program.
 
-const int8_t NO_SWITCH = 0; // No limit switch is pressed.
-const int8_t FRONT_SWITCH = 1; // The front limit switch is pressed.
-const int8_t BACK_SWITCH = 2; // The back limit switch is pressed.
-int8_t memSwitch = 0; // Track what the switch was set to when last checked.
+portMUX_TYPE frontSwitchChangeMutex = portMUX_INITIALIZER_UNLOCKED; // Spinlock mutex.
+portMUX_TYPE backSwitchChangeMutex = portMUX_INITIALIZER_UNLOCKED; // Spinlock mutex.
+bool frontSwitchChanged = false;
+bool backSwitchChanged = false;
+
+/**
+ * @brief Interrupt service routine (ISR) for for front limit switch.
+ * @details This code runs every time the value of the GPIO pin connected to 
+ * the front bumper changes meaning that it has either been pressed or released.
+ * The switch uses on internal pull-down resitor to prevent it being a flaoting
+ * input pin.
+ * 
+ * # Running in IRAM
+ * 
+ * Note that this code is loaded into IRAM (rather than the usual DRAM) using 
+ * the IRAM_ATTR attribute in its declaration. This means that it is accessed 
+ * via the instruction bus for fast and consistent execution times.
+ *  
+ * @param null.
+ * @return null.   
+ ******************************************************************************/
+void IRAM_ATTR isrFrontSwitchChanged() 
+{
+   portENTER_CRITICAL(&frontSwitchChangeMutex);   
+   frontSwitchChanged = true;
+   portEXIT_CRITICAL(&frontSwitchChangeMutex);   
+} // isrFrontSwitchChanged() 
+
+/**
+ * @brief Interrupt service routine (ISR) for for back limit switch.
+ * @details This code runs every time the value of the GPIO pin connected to 
+ * the back bumper changes meaning that it has either been pressed or released.
+ * The switch uses on internal pull-down resitor to prevent it being a flaoting
+ * input pin.
+ * 
+ * # Running in IRAM
+ * 
+ * Note that this code is loaded into IRAM (rather than the usual DRAM) using 
+ * the IRAM_ATTR attribute in its declaration. This means that it is accessed 
+ * via the instruction bus for fast and consistent execution times.
+ *  
+ * @param null.
+ * @return null.   
+ ******************************************************************************/
+void IRAM_ATTR isrBackSwitchChanged() 
+{
+   portENTER_CRITICAL(&backSwitchChangeMutex);   
+   backSwitchChanged = true;
+   portEXIT_CRITICAL(&backSwitchChangeMutex);    
+} // isrBackSwitchChanged() 
 
 /**
  * @brief Set up the limit switches used to detect when the robot falls over.
  * @details The robot has front and  back limit switches that contact the 
  * ground when the robot leans too far forward or backward. This routine
  * initializes the GPIO pins connected to those limit switches as input and 
- * configures the pins with a weak pullup resistor.   
- * ==========================================================================*/
+ * configures the pins with a weak pullup resistor. 
+ * 
+ * # ISRs on ESP32
+ * 
+ * https://deepbluembedded.com/esp32-external-interrupts-pins-arduino-examples/
+ * @param null.
+ * @return null.   
+ *   
+ ******************************************************************************/
 void setupLimitSwitches()
 {
    Log.traceln("<setupLimitSwitches> Set weak pullup resistor for GPIO pin %d (forward limit switch) and %d (backward limit switch).", G_FRONT_BUMPER, G_BACK_BUMPER);
-   pinMode(G_FRONT_BUMPER, INPUT_PULLUP); // Set GPIO pin to input.
-   pinMode(G_BACK_BUMPER, INPUT_PULLUP); // Set GPIO pin to input.
+   pinMode(G_FRONT_BUMPER, INPUT_PULLDOWN); // Front bumper input with pulldown.
+   pinMode(G_BACK_BUMPER, INPUT_PULLDOWN); // Back bumper input with pulldown.
+   attachInterrupt(G_FRONT_BUMPER, isrFrontSwitchChanged, CHANGE); // ISR for front bumper.
+   attachInterrupt(G_BACK_BUMPER, isrBackSwitchChanged, CHANGE); // ISR for back bumper.
 } // setupLimitSwitches()
 
 /**
  * @brief Check to see if the specified switch is contacting the ground.
-==============================================================================*/
+ ******************************************************************************/
 void checkLimitSwitches()
 {
-   if((digitalRead(G_FRONT_BUMPER) == false)) // Front limit switch pressed?
+   if(frontSwitchChanged)
    {
-      if(memSwitch != FRONT_SWITCH) // Was not pressed during last check?
-      {
-         Log.verboseln("<checkLimitSwitches> Front limit switch tripped. memSwitch = %d", memSwitch);
-         memSwitch = FRONT_SWITCH;
-         saveRgbColour();
-         setStdRgbColour(PINK);
-         return;
-      } //if
-      else // Was pressed during last check. No need to do anything further.
-      {
-         return;
-      } // else
-   } //if
-   if(digitalRead(G_BACK_BUMPER) == false) // Back limit switch pressed?
-   {
-      if(memSwitch != BACK_SWITCH) // Was not pressed during last check?
-      {
-         Log.verboseln("<checkLimitSwitches> Back limit switch tripped. memSwitch = %d", memSwitch);
-         memSwitch = BACK_SWITCH;
-         saveRgbColour();
-         setStdRgbColour(AQUA); // Temporarily set RGB LED to indicate robot lean
-         return;
-      } // if
-      else // Was pressed during last check. No need to do anything further.
-      {
-         return;
-      } // else
-   } // if 
-   if(memSwitch != NO_SWITCH) // No switched pressed this time nor the last time we checked. 
-   {
-      Log.verboseln("<checkLimitSwitches> No limit switch tripped. memSwitch = %d", memSwitch);
-      memSwitch = NO_SWITCH; // Track the fact that now no bumper switches are pressed.
-      loadRgbColour(); // Set RGB LED back to colour it was before the switches tripped.
+      Log.verboseln("<checkLimitSwitches> Front limit switch changed.");
+      frontSwitchChanged = false;
+//      saveRgbColour();
+//      setStdRgbColour(PINK);
    } // if
-   return;
+   if(backSwitchChanged)
+   {
+      Log.verboseln("<checkLimitSwitches> Back limit switch changed.");
+      portENTER_CRITICAL(&backSwitchChangeMutex);   
+      backSwitchChanged = false;
+      portEXIT_CRITICAL(&backSwitchChangeMutex); 
+//      saveRgbColour();
+//      setStdRgbColour(AQUA);
+   } // if
 } // checkLimitSwitches()
 
 #endif // End of precompiler protected code block
